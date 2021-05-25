e4d9acc3f07736e952a58e1eae5b71d0f314e0a5
Moumen Hallak <moumen95@gmail.com>
2021-05-25 14:23:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ class Tree {
     this.species = species;
   }
   static definition(){
-    return `A tree is a perennial plant with an elongated stem, or trunk, supportingbranches and leaves.`;
+    return `A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.`;
   }
 }

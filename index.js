758463bafd7d526bb2e84d816ffa1ce64ecf96e7
Moumen Hallak <moumen95@gmail.com>
2021-05-25 14:30:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ class Deciduous extends Tree {
     this.name = name;
   }
   static definition(){
+    return `${super.definition()} Deciduous trees shed their leaves annually.`;
+  }
+}
+
+class Deciduous extends Tree {
+  constructor(species, name){
+    super(species);
+    this.name = name;
+  }
+  static definition(){
     return `${super.definition()} Deciduous trees shed their leaves annually.`
   }
 }
